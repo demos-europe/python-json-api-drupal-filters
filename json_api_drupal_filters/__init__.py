964eb5c8fa8b1d5f0d27cba0bc800fce14f6c244
Noah Kamara <kamara@demos-deutschland.de>
2024-07-30 11:24:33 +0200

    fix: add imports to __all__

--- json_api_drupal_filters/__init__.py
+++ json_api_drupal_filters/__init__.py
@@ -1,2 +1,5 @@
 from .filter_parser import FilterParser
-from .filter_errors import *
+from .drupal_filter import DrupalFilter
+from .filter_errors import NoGroupOrCondition, RootKeyUsedError
+
+__all__ = ["FilterParser", "DrupalFilter", "NoGroupOrCondition", "RootKeyUsedError"]
