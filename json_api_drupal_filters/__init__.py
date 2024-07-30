debe9a4c98bd5693383a9db65929f161b0794dd3
Noah Kamara <kamara@demos-deutschland.de>
2024-07-30 14:01:47 +0200

    fix: expose filter error from __init__

--- json_api_drupal_filters/__init__.py
+++ json_api_drupal_filters/__init__.py
@@ -1,5 +1,5 @@
 from .filter_parser import FilterParser
 from .drupal_filter import DrupalFilter
-from .filter_errors import NoGroupOrCondition, RootKeyUsedError
+from .filter_errors import FilterError, NoGroupOrCondition, RootKeyUsedError
 
-__all__ = ["FilterParser", "DrupalFilter", "NoGroupOrCondition", "RootKeyUsedError"]
+__all__ = ["FilterParser", "DrupalFilter", "FilterError", "NoGroupOrCondition", "RootKeyUsedError"]
